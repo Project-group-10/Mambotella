f1ab654233fa923315078eb2a1f763202e5d9ac3
Allan39543 <warigiachegesa@gmail.com>
2022-08-15 15:33:52 +0400

    readmore btn

---
+++ stories.js
@@ -0,0 +1,50 @@
+//stories readmore
+var readmore_content=document.querySelector('.Readmore')
+var readmore_btn=document.querySelector('.readmorebtn')
+readmore_btn.addEventListener('click', displaycontent);
+function displaycontent(){
+    readmore_content.style.display='block'
+    readmore_btn.style.display='none'
+}
+//second
+var readmore_contentf=document.querySelector('.Readmoref')
+var readmore_btnf=document.querySelector('.readmorebtnf')
+readmore_btnf.addEventListener('click', displaycontentf);
+function displaycontentf(){
+    readmore_contentf.style.display='block'
+    readmore_btnf.style.display='none'
+}
+
+//third
+var readmore_contents=document.querySelector('.Readmores')
+var readmore_btns=document.querySelector('.readmorebtns')
+readmore_btns.addEventListener('click', displaycontents);
+function displaycontents(){
+    readmore_contents.style.display='block'
+    readmore_btns.style.display='none'
+}
+
+//fourth
+var readmore_contenta=document.querySelector('.Readmorea')
+var readmore_btna=document.querySelector('.readmorebtna')
+readmore_btna.addEventListener('click', displaycontenta);
+function displaycontenta(){
+    readmore_contenta.style.display='block'
+    readmore_btna.style.display='none'
+}
+//fifth
+var readmore_contentr=document.querySelector('.Readmorer')
+var readmore_btnr=document.querySelector('.readmorebtnr')
+readmore_btnr.addEventListener('click', displaycontentr);
+function displaycontentr(){
+    readmore_contentr.style.display='block'
+    readmore_btnr.style.display='none'
+}
+//sixth
+var readmore_contentj=document.querySelector('.Readmorej')
+var readmore_btnj=document.querySelector('.readmorebtnj')
+readmore_btnj.addEventListener('click', displaycontentj);
+function displaycontentj(){
+    readmore_contentj.style.display='block'
+    readmore_btnj.style.display='none'
+}
